eb73c11cf6fe5f718f3216399912438d99a9b81d
Enlysure <Enlysure@gmail.com>
2025-07-02 21:50:00 +0800

    ✨ 改进(command.py): 优化新成员加入时的验证流程，缩短验证时间至3分钟并调整踢出逻辑。

--- src/tgbot/command.py
+++ src/tgbot/command.py
@@ -71,37 +71,38 @@ class Command:
         chat_data = await SystemOperate.get_chat_verify_info(chat_id)
         if chat_data is None:
             await context.bot.send_message(chat_id=chat_id,
-                                           text=f"您好,{update.effective_user.first_name}，欢迎加入本群！\n"
-                                                f"本群暂未配置验证功能。")
+                                           text=f"您好,{update.effective_user.first_name}，欢迎加入本群！\n")
             return
         state = base64.b64encode(f"{user_id}#{chat_id}".encode()).decode()
         url = (
             f"https://github.com/login/oauth/authorize?client_id={Config.CLIENT_ID}&redirect_uri={Config.REDIRECT_URI}&scope=user"
             f"&state={state}")
         button = InlineKeyboardMarkup([[InlineKeyboardButton(text="点我进行验证", url=url)]])
-        await context.bot.send_message(chat_id=chat_id, text=f"您好,{update.effective_user.first_name}，欢迎加入本群！\n"
-                                                             f"为了保证群内环境，需要验证您的身份。\n"
-                                                             f"您需要star此项目GitHub仓库,然后点击下面的按钮登录进行验证。"
-                                                             f"仓库地址：https://github.com/{chat_data.path}\n"
-                                                             f"请点击以下按钮跳转到网页进行验证,限时为5分钟",
-                                       reply_markup=button)
+        join_msg = await context.bot.send_message(chat_id=chat_id,
+                                                  text=f"您好,{update.effective_user.first_name}，欢迎加入本群！\n"
+                                                       f"为了保证群内环境，需要验证您的身份。\n"
+                                                       f"您需要star此项目GitHub仓库,然后点击下面的按钮登录进行验证。"
+                                                       f"仓库地址：https://github.com/{chat_data.path}\n"
+                                                       f"请点击以下按钮跳转到网页进行验证,限时为3分钟",
+                                                  reply_markup=button)
         # 对用户设置禁言
         await context.bot.restrict_chat_member(chat_id=chat_id, user_id=user_id,
                                                permissions={"can_send_messages": False,
                                                             "can_send_polls ": False,
                                                             "can_send_other_messages": False})
         await UserOperate.add_user_verify_info(user_id, chat_id)
-        # 设置定时任务，5分钟后未通过直接踢出
-        await sleep(60 * 5)
+        # 未通过直接踢出
+        await join_msg.delete()
+        await sleep(60 * 3)
         user_data = await UserOperate.get_user_info(user_id, chat_id)
         if user_data is None:
-            await context.bot.kick_chat_member(chat_id=chat_id, user_id=user_id)
+            await context.bot.ban_chat_member(chat_id=chat_id, user_id=user_id)
             return
         user_data.failed_times += 1
         await UserOperate.update_user_info(user_data)
         if user_data.failed_times >= 5:
             await context.bot.banChatMember(chat_id=chat_id, user_id=user_id)
-        await context.bot.kick_chat_member(chat_id=chat_id, user_id=user_id)
+        await context.bot.ban_chat_member(chat_id=chat_id, user_id=user_id)
 
     @staticmethod
     async def bind(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
